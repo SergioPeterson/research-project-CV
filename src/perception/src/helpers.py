4f991cec0deba7339dc725c9a4c5d546b442a4e1
jacksonhilton <jacksonhilton@berkeley.edu>
2023-12-01 13:00:37 -0800

    Added classes.py and helpers.py

---
+++ src/perception/src/helpers.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+from lab4_cam.srv import ImageSrv, ImageSrvResponse
+import cv2, time, sys
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from numpy.linalg import *
+
+############################ FROM HOW CARS SEE PAPER ###################################
+## https://github.com/kenshiro-o/CarND-Advanced-Lane-Lines/blob/master/notebook.ipynb ##
+
+def ros_to_np_img(ros_img_msg):
+  # Create a CvBridge to convert ROS messages to OpenCV images
+  bridge = CvBridge()
+  return np.array(bridge.imgmsg_to_cv2(ros_img_msg,'bgr8'))
+
+def threshold_img(img, channel, thres=(0, 255)):
+    """
+    Applies a threshold mask to the input image
+    """
+    img_ch = img[:,:,channel]
+    if thres is None:  
+        return img_ch
+    
+    mask_ch = np.zeros_like(img_ch)
+    mask_ch[ (thres[0] <= img_ch) & (thres[1] >= img_ch) ] = 1
+    return mask_ch
+
+def abs_sobel(gray_img, x_dir=True, kernel_size=3, thres=(0, 255)):
+    """
+    Applies the sobel operator to a grayscale-like (i.e. single channel) image in either horizontal or vertical direction
+    The function also computes the asbolute value of the resulting matrix and applies a binary threshold
+    """
+    sobel = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0, ksize=kernel_size) if x_dir else cv2.Sobel(gray_img, cv2.CV_64F, 0, 1, ksize=kernel_size) 
+    sobel_abs = np.absolute(sobel)
+    sobel_scaled = np.uint8(255 * sobel / np.max(sobel_abs))
+    
+    gradient_mask = np.zeros_like(sobel_scaled)
+    gradient_mask[(thres[0] <= sobel_scaled) & (sobel_scaled <= thres[1])] = 1
+    return gradient_mask
+
+def dir_sobel(gray_img, kernel_size=3, thres=(0, np.pi/2)):
+    """
+    Computes sobel matrix in both x and y directions, gets their absolute values to find the direction of the gradient
+    and applies a threshold value to only set pixels within the specified range
+    """
+    sx_abs = np.absolute(cv2.Sobel(gray_img, cv2.CV_64F, 1, 0, ksize=kernel_size))
+    sy_abs = np.absolute(cv2.Sobel(gray_img, cv2.CV_64F, 0, 1, ksize=kernel_size))
+    
+    dir_sxy = np.arctan2(sx_abs, sy_abs)
+
+    binary_output = np.zeros_like(dir_sxy)
+    binary_output[(dir_sxy >= thres[0]) & (dir_sxy <= thres[1])] = 1
+    
+    return binary_output
+
+def combined_sobels(sx_binary, sy_binary, sxy_magnitude_binary, gray_img, kernel_size=3, angle_thres=(0, np.pi/2)):
+    sxy_direction_binary = dir_sobel(gray_img, kernel_size=kernel_size, thres=angle_thres)
+    
+    combined = np.zeros_like(sxy_direction_binary)
+    # Sobel X returned the best output so we keep all of its results. We perform a binary and on all the other sobels    
+    combined[(sx_binary == 1) | ((sy_binary == 1) & (sxy_magnitude_binary == 1) & (sxy_direction_binary == 1))] = 1
+    
+    return combined
+
+def perspective_transform(img, src, dst):   
+    """
+    Applies a perspective 
+    """
+    M = cv2.getPerspectiveTransform(src, dst)
+    img_size = (img.shape[1], img.shape[0])
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+    
+    return warped
